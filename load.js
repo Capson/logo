8ae492c3a8ed5f5acfd496764d46d70115c7c919
Simarpreet Singh <ssimarpreet11@gmail.com>
2024-05-18 15:46:46 +0530

    Update load.js

--- load.js
+++ load.js
@@ -1,3 +1,12 @@
+if(window.location.hostname==='popify.dorik.io')
+{
 window.onload = function() {
     document.querySelector('a').innerHTML = '<img src="https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/undraw_mobile_pay_9abj_ww1tsmp8.svg" alt="Weblytics" width="174px">';
 };
+}
+else
+{
+    window.onload = function() {
+    document.querySelector('a').innerHTML = '<img src="https://cdn.dorik.com/61064130a87f8e00119f0c55/626e3f1584c9110011f0e82d/images/Copy-of-Copy-of-AS-Marketplace-CompanyName-1280x720px-(1920-x-1080-px)-(1)_lt5b4quy.svg" alt="Weblytics" width="174px">';
+};
+}
