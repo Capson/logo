6d477e0b66717abff45c74ab822355cd8ce8d6ba
Simarpreet Singh <ssimarpreet11@gmail.com>
2024-05-18 15:21:22 +0530

    Update load.js

--- load.js
+++ load.js
@@ -1,3 +1,1 @@
-<script>
-  document.querySelector('a').innerHTML='<img src="https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/undraw_mobile_pay_9abj_ww1tsmp8.svg" alt="Weblytics" width="174px">'
-</script>
+document.querySelector('a').innerHTML='<img src="https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/undraw_mobile_pay_9abj_ww1tsmp8.svg" alt="Weblytics" width="174px">'
